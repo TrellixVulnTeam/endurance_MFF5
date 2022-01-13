496c5e9f14ea41a0c46d4835b3cd8a86389746a2
Lucas Guilherme <lguilherme8@gmail.com>
2022-01-12 21:16:14 -0300

    implemented the home gallery animation

--- app/pages/Home/index.js
+++ app/pages/Home/index.js
@@ -1,5 +1,6 @@
 import Page from 'components/Page'
 
+import gsap, { Power4 } from 'gsap'
 export default class extends Page {
   constructor () {
     super({
@@ -8,7 +9,9 @@ export default class extends Page {
       },
       element: '.home',
       elements: {
-        wrapper: '.home__content'
+        wrapper: '.home__content',
+        galleryWrapper: '.home__gallery__wrapper',
+        titlesWrapper: '.home__titles__wrapper'
       }
     })
   }
@@ -18,6 +21,20 @@ export default class extends Page {
    */
   async show (url) {
     this.element.classList.add(this.classes.active)
+
+    gsap.from(this.elements.galleryWrapper, {
+      left: -150 + 'vw',
+      duration: 3,
+      delay: 6,
+      ease: Power4.easeOut
+    })
+
+    gsap.from(this.elements.titlesWrapper, {
+      left: -4000,
+      duration: 3,
+      delay: 6,
+      ease: Power4.easeOut
+    })
 
     return super.show(url)
   }
